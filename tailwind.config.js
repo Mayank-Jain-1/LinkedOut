9595fea92492de38f5504fa85bd5e29a07b6f46a
Mayank <kaushalmayank.k@gmail.com>
2023-02-27 03:20:32 +0530

    added tailwind and made halg login page

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#7d6a9c",
+        secondary: "#c9c3d4",
+        faded: "#e0dbe9"
+      }
+    },
+  },
+  plugins: [],
+};
